812f9f67fe6dbd96ee716e324a09290c1ab6b923
Coder731 <stephenobrien1@gmail.com>
2021-07-19 20:12:18 +0000

    update search box

--- wikipediacrawler/sketch.js
+++ wikipediacrawler/sketch.js
@@ -1,6 +1,16 @@
 // https://en.wikipedia.org/w/api.php?action=opensearch
 // https://en.wikipedia.org/w/api.php?action=query&prop
 
+let userInput;
+
 function setup() {
     noCanvas();
+    userInput = select('#userinput');
+    userInput.changed(goWiki);
+    goWiki();
+
+    function goWiki() {
+        let term = userInput.value();
+        console.log(term);
+    }
 }
